a9214fb302ccda3d098baf2aa5490501b6e1d468
Graham Zemel <69569628+grahamzemel@users.noreply.github.com>
2022-11-19 13:17:18 -0500

    tailwind bug

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import App from './App.svelte';
+import App from './routes/+layout.svelte';
 
 const app = new App({
 	target: document.body,
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let name;
+</script>
+
+<div>
+    <h1 class="text-[#505050]">Hello {name}!</h1>
+    
+</div>
