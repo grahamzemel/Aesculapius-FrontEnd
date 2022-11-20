5ea17e541736abbddb2d61556db26a5797d41be4
Graham Zemel <69569628+grahamzemel@users.noreply.github.com>
2022-11-20 02:13:15 -0500

    Updated medications

--- src/routes/drugfacts.svelte
+++ src/routes/drugfacts.svelte
@@ -7,155 +7,48 @@
   <div class="container px-5 py-24 mx-auto">
     <div class="text-center mb-20">
       <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">Medications!</h1>
-      <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Here are a few medications you might find helpful in specific areas of health and well-being.</p>
+      <p class="text-[#b7babe] leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Here are a few medications you might find helpful in specific areas of health and well-being.</p>
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
-        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">Pain relief</h2>
-        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-          <DrugPoint drugLink="google.com" drugName="test"/>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Third Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fourth Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fifth Link
-          </a>
-        </nav>
+        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">PAIN RELIEF</h2>
+        <div class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+          <DrugPoint drugLink="https://www.webmd.com/drugs/2/drug-362/acetaminophen+oral/details" drugName="Tylenol"/>
+          <DrugPoint drugLink="https://www.webmd.com/drugs/mono-9368-IBUPROFEN+-+ORAL.aspx?drugid=5166&drugname=ibuprofen+oral" drugName="Ibuprofen"/>
+          <DrugPoint drugLink="https://www.webmd.com/drugs/2/drug-17549/lidoderm-topical/details" drugName="Litoderm"/>
+          <DrugPoint drugLink="https://www.webmd.com/drugs/2/drug-7420-553/codeine-sulfate/details" drugName="Codeine"/>
+        </div>
       </div>
       <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
-        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">THE 400 BLOWS</h2>
-        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>First Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Second Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Third Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fourth Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fifth Link
-          </a>
-        </nav>
+        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">VACCINES</h2>
+        <div class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <DrugPoint drugLink="https://www.cdc.gov/vaccines/parents/diseases/flu.html" drugName="Influenza (Flu)"/>
+            <DrugPoint drugLink="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/stay-up-to-date.html" drugName="Covid-19"/>
+            <DrugPoint drugLink="https://www.cdc.gov/vaccines/parents/diseases/hpv.html" drugName="HPV"/>
+            <DrugPoint drugLink="https://www.cdc.gov/vaccines/parents/diseases/tetanus.html" drugName="Tetanus"/>
+
+        </div>
       </div>
       <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
-        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">THE CATALYZER</h2>
-        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>First Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Second Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Third Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fourth Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fifth Link
-          </a>
-        </nav>
+        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">ALLERGEN MEDICATIONS</h2>
+        <div class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <DrugPoint drugLink="https://www.allegra.com/en-us/products/allergy-relief/24-hour-allergy-relief" drugName="Allegra"/>
+            <DrugPoint drugLink="https://www.benadryl.com/" drugName="Benadryl"/>
+            <DrugPoint drugLink="https://www.zyrtec.com/" drugName="Zyrtec"/>
+            <DrugPoint drugLink="https://www.claritin.com/products/claritin" drugName="Claritin"/>
+        </div>
       </div>
       <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
-        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">NEPTUNE</h2>
-        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>First Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Second Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Third Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fourth Link
-          </a>
-          <a>
-            <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Fifth Link
-          </a>
-        </nav>
+        <h2 class="font-medium title-font tracking-widest text-gray-900 mb-4 text-sm text-center sm:text-left">FURTHER RESOURCES</h2>
+        <div class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <DrugPoint drugLink="https://www.webmd.com/drugs/2/index" drugName="WebMD"/>
+            <DrugPoint drugLink="https://www.fda.gov/drugs/information-consumers-and-patients-drugs/find-information-about-drug" drugName="FDA"/>
+            <DrugPoint drugLink="https://patient.info/medicine" drugName="Patient.info"/>
+            <DrugPoint drugLink="https://www.drugs.com/drug_information.html" drugName="Drugs.com"/>
+            <DrugPoint drugLink="https://kidshealth.org/en/kids/kidmedic.html" drugName="KidsHealth"/>
+            <DrugPoint drugLink="https://www.who.int/" drugName="WHO"/>
+
+        </div>
       </div>
     </div>
     <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
