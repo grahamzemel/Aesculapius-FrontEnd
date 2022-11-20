5007107322fabc92ac839e3d905f63d25605c6e5
BestLocation <115041500+BestLocation@users.noreply.github.com>
2022-11-19 22:40:27 -0500

    Update home.svelte

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -12,14 +12,15 @@
         <h1>How was this helpful tool <span class="underline decoration-slate-500">created</span>?</h1>
         
         <p class="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
+            We used NLP by CoHere and multiple API calls to create our chatbot. We used Sverte to create the webpage.
         </p>
         
         <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-            <FeaturedCard title="elegance" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet" link="/"/>
-            <FeaturedCard title="elegance" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet" link="/"/>
-            <FeaturedCard title="elegance" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet" link="/"/>
-            <FeaturedCard title="elegance" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet" link="/"/>
+            <FeaturedCard title="Purpose" desc="Our Purpose is to make online medical information more accessible to the less Tech Savy and anyone who cant find important health information online" link="/"/>
+            <FeaturedCard title="Importance" desc="We belive our Bot can have a real impact by broading access to medical information" link="/"/>
+            <FeaturedCard title="Future" desc="If this bot became a full product, we could add more features such as checking the avrage cost of a procedure across different nearbye hospitals. We could also turn the bot into a webapp that could be used on mobile phones" link="/"/>
+            
+         
         </div>
     </div>
 </section>
