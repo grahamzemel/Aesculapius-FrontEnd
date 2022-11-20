ec3987c24c760924f35ee5dfea2b19604b99fa8e
Graham Zemel <69569628+grahamzemel@users.noreply.github.com>
2022-11-20 02:26:59 -0500

    updated cards

--- src/components/FeaturedCard.svelte
+++ src/components/FeaturedCard.svelte
@@ -4,12 +4,6 @@
     export let link;
 </script>
 <div class="p-8 space-y-3 border-2 border-slate-400 dark:border-slate-300 rounded-xl">
-    <span class="inline-block text-slate-500 dark:text-slate-400">
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-        </svg>
-    </span>
-
     <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">{title}</h1>
 
     <p class="text-gray-500 dark:text-gray-300">
